6b733f89e771e04fcd1ef88bb8a6839d54eb45f8
jonesbonesjrs <jonasleonas1234@gmail.com>
2020-08-02 23:23:30 -0500

    Updates

---
+++ precache-manifest.f4d38aa8dbbd855c78845e37271378b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a251e7f3e50e5c2c273c6182e49b07",
+    "url": "/new-ap/index.html"
+  },
+  {
+    "revision": "2fcbe47fcca4f6b893f7",
+    "url": "/new-ap/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c778527e0bc664dbc138",
+    "url": "/new-ap/static/js/2.ebe65a63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-ap/static/js/2.ebe65a63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fcbe47fcca4f6b893f7",
+    "url": "/new-ap/static/js/main.b0c4e96a.chunk.js"
+  },
+  {
+    "revision": "25711ac0c04640d1c0ed",
+    "url": "/new-ap/static/js/runtime-main.c5720eb4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-ap/static/media/logo.5d5d9eef.svg"
+  }
+]);
